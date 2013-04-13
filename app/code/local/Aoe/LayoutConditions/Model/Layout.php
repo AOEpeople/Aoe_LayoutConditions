54c5aebfb9580d4e876f37f23c1a2a960bd50338
Jonathan Gautheron <jgautheron@nexway.com>
2013-04-13 19:10:10 +0200

    Implement ifconfig & unlessconfig for <remove>

--- app/code/local/Aoe/LayoutConditions/Model/Layout.php
+++ app/code/local/Aoe/LayoutConditions/Model/Layout.php
@@ -63,5 +63,57 @@ class Aoe_LayoutConditions_Model_Layout extends Mage_Core_Model_Layout {
 		return parent::_generateAction($node, $parent);
 	}
 
+    /**
+     * Layout xml generation
+     *
+     * @return Mage_Core_Model_Layout
+     */
+    public function generateXml()
+    {
+        $xml = $this->getUpdate()->asSimplexml();
+        $removeInstructions = $xml->xpath("//remove");
+        if (is_array($removeInstructions)) {
+            foreach ($removeInstructions as $infoNode) {
+                $attributes = $infoNode->attributes();
+                $blockName = (string)$attributes->name;
 
+                // changes start here
+                $ifConfigPath = isset($attributes->ifconfig) ? (string)$attributes->ifconfig : false;
+                if ($ifConfigPath && !Mage::getStoreConfigFlag($ifConfigPath)) {
+                    continue;
+                }
+
+                $unlessConfigPath = isset($attributes->unlessconfig) ? (string)$attributes->unlessconfig : false;
+                if ($unlessConfigPath && Mage::getStoreConfigFlag($unlessConfigPath)) {
+                    continue;
+                }
+                // changes end here
+
+                if ($blockName) {
+                    $ignoreNodes = $xml->xpath("//block[@name='".$blockName."']");
+                    if (!is_array($ignoreNodes)) {
+                        continue;
+                    }
+                    $ignoreReferences = $xml->xpath("//reference[@name='".$blockName."']");
+                    if (is_array($ignoreReferences)) {
+                        $ignoreNodes = array_merge($ignoreNodes, $ignoreReferences);
+                    }
+
+                    foreach ($ignoreNodes as $block) {
+                        if ($block->getAttribute('ignore') !== null) {
+                            continue;
+                        }
+                        if (($acl = (string)$attributes->acl) && Mage::getSingleton('admin/session')->isAllowed($acl)) {
+                            continue;
+                        }
+                        if (!isset($block->attributes()->ignore)) {
+                            $block->addAttribute('ignore', true);
+                        }
+                    }
+                }
+            }
+        }
+        $this->setXml($xml);
+        return $this;
+    }
 }
